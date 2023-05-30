88e6ab4516c28de36b71fdf7b1d332eb3b1e5fee
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-05-30 11:31:12 +0300

    Create main.rb

---
+++ main.rb
@@ -0,0 +1,21 @@
+class Main
+    def display_menu
+      puts "Welcome to our catalog 👋\nSelect an option:"
+      options = [
+        '1 - List all books',
+        '2 - List all music albums',
+        '3 - List all movies',
+        '====================================',
+        '4 - List all genres',
+        '5 - List all labels',
+        '6 - List all authors',
+        '====================================',
+        '7 - Add a book',
+        '8 - Add a music album',
+        '9 - Add a movie',
+        '10 - Quit'
+      ]
+      puts options
+    end
+  
+  end
