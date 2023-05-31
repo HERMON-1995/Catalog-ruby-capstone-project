0fa784537975d6fb4ae11a16269ab9e8b3040e4a
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-05-31 12:42:24 +0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,137 +1,111 @@
 require_relative 'book'
 require_relative 'label'
+require_relative 'music_album'
+require_relative 'music_album_store'
+require 'json'
 
-books = []
-labels = []
-@albums = []
-@genres = []
+class App
+  attr_reader :books, :labels
 
-def add_book(books)
-  puts 'Enter book publisher:'
-  publisher = gets.chomp
-  puts 'Enter cover state (good/bad):'
-  cover_state = gets.chomp
-  book = Book.new(publisher, cover_state)
-  books << book
-  book
-end
-
-def list_books(books)
-  puts 'List of all books:'
-  books.each do |book|
-    puts "Book ID: #{book.id},
-      Publisher: #{book.publisher},
-      Cover State: #{book.cover_state},
-      Archived: #{book.archived}"
+  def initialize
+    @books = []
+    @labels = []
   end
-end
 
-def add_label(labels)
-  puts 'Enter label title:'
-  title = gets.chomp
-  puts 'Enter label color:'
-  color = gets.chomp
-  label = Label.new(title, color)
-  labels << label
-  label
-end
-
-def list_labels(labels)
-  puts 'Listing all labels:'
-  labels.each do |label|
-    puts "Label #{label.id}: #{label.title} (#{label.color})"
-  end
-end
-
-loop do
-  puts 'Enter an option:'
-  puts '1. Add a book'
-  puts '2. List all books'
-  puts '3. Add a label'
-  puts '4. List all labels'
-  puts '5. Exit'
-  choice = gets.chomp.downcase
-
-  case choice
-  when '1'
-    add_book(books)
-    puts 'Book Added successfully'
-  when '2'
-    list_books(books)
-  when '3'
-    add_label(labels)
-    puts 'Label Added successfully'
-  when '4'
-    list_labels(labels)
-  when '5'
-    break
-  else
-    puts 'Invalid choice. Please try again.'
-  end
-end
-
-def list_all_music_albums
-  if @albums.empty?
-    puts "\nNo Albums to show. Please add some Musid Albums . . . ".magenta
-  else
-    puts "\nAvailable Music Albums in the list: #{@albums.count} \n".magenta
-    @albums.each_with_index do |album, index|
-      print "[ #{index + 1} ]:  Music Album: #{album.album_name} | Author: #{album.author.first_name} "
-      print "#{album.author.last_name} |  Label: #{album.label.title}  | Archived: #{album.archived} | "
-      puts "On Spotify: #{album.on_spotify} | Publication_date: #{album.publish_date} | Genre: #{album.genre.name}"
-    end
-  end
-end
-
-def add_music_album
-  print 'Enter the name of the music album: '
-  album_name = gets.chomp
-  print 'Enter the publish date of the music album e.g (2023-01-11) '
-  date = set_valid_date
-
-  album = MusicAlbum.new(nil, album_name, date)
-
-  archive_album(album)
-
-  author = add_author
-  author.add_item(album)
-  puts "\nAuthor added for the album #{album.album_name} succesfully ".green
-
-  label = add_label
-  label.add_item(album)
-  puts "\nLabel added for the album #{album.album_name} successfully ".green
-
-  genre = add_genre
-  genre.add_item(album)
-  puts "\nGenre added for the album #{album.album_name} successfully ".green
-end
-
-def get_user_input(prompt, valid_responses)
-  while true
+  def get_input(prompt, type = :to_s)
     print prompt
     input = gets.chomp
-    break if valid_responses.include?(input)
-
-    puts "\nInvalid input. Please enter one of the following: #{valid_responses.join(', ')} \n".red
+    type == :to_i ? input.to_i : input
   end
-  input
-end
 
-def list_all_genres
-  if @genres.empty?
-    puts "\nNo Genres to Show Add some genres . . . ".magenta
-  else
-    puts "\nAvailable Genres in the list : #{@genres.count} \n".magenta
-    @genres.each_with_index do |genre, index|
-      print "[ #{index + 1} ]: Name: #{genre.name} \n"
+  def list_labels
+    puts 'Listing all labels:'
+    labels.each do |label|
+      puts "Label #{label.id}: #{label.title} (#{label.color})"
+    end
+  end
+
+  def add_label
+    puts 'Enter label title:'
+    title = gets.chomp
+    puts 'Enter label color:'
+    color = gets.chomp
+    label = Label.new(title, color)
+    labels << label
+    label
+  end
+
+  def save_books(filename)
+    File.write(filename, JSON.generate(books.map(&:to_h)))
+  end
+
+  def save_labels(filename)
+    File.write(filename, JSON.generate(labels.map(&:to_h)))
+  end
+
+  def book_options
+    loop do
+      puts 'Enter an option:'
+      puts '1. Add a book'
+      puts '2. List all books'
+      puts '3. Add a label'
+      puts '4. List all labels'
+      puts '5. Exit'
+      choice = gets.chomp.downcase
+
+      case choice
+      when '1'
+        add_book(books)
+        save_books('data/books.json')
+        puts 'Book added successfully'
+      when '2'
+        list_books(books)
+      when '3'
+        add_label
+        save_labels('data/labels.json')
+        puts 'Label added successfully'
+      when '4'
+        list_labels
+      when '5'
+        puts 'Thanks for using.'
+        return
+      else
+        puts 'Invalid choice. Please try again.'
+      end
+    end
+  end
+
+  def main_menu
+    puts "Welcome to our catalog 👋\nSelect an option:"
+    options = [
+      '1 - List books options',
+      '====================================',
+      '2 - List music options',
+      '====================================',
+      '3 - List game options',
+      '====================================',
+      '4 - Quit'
+    ]
+    puts options
+    option = gets.chomp.downcase
+    case option
+    when '1'
+      book_options
+    when '2'
+      store = MusicAlbumStore.new('./data/albums.json')
+      store.run
+    when '3'
+      puts 'Thanks for using.'
+      nil
+    else
+      puts 'Invalid choice. Please try again.'
     end
   end
 end
 
-def create_genre
-  print 'Enter the name of the genre: '
-  name = gets.chomp
-  genre = Genre.new(name)
-  @genres << genre
-  genre
+def main_menu
+  app = App.new
+  app.main_menu
 end
+
+main_menu
