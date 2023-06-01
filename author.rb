32a5d4b11bdfd18cd2c317897115c6f83a9e801f
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-06-01 17:15:03 +0300

    Update author.rb

--- author.rb
+++ author.rb
@@ -1,15 +1,50 @@
-class Author
-  attr_accessor :first_name, :last_name, :id, :items
+require_relative 'item'
 
-  def initialize(first_name, last_name, id: nil)
-    @id = id.nil? ? Random.rand(1..1000) : id
+class Author < Item
+  attr_reader :id, :first_name, :last_name, :items
+
+  @id_counter = 0
+
+  def initialize(first_name = '', last_name = '', _items = [])
+    super()
+    @id_counter ||= 0
+    @id_counter += 1
+    @id = @id_counter
     @first_name = first_name
     @last_name = last_name
     @items = []
   end
 
+  def self.json_create(object)
+    new(object['first_name'], object['last_name'], object['items'])
+  end
+
   def add_item(item)
-    @items << item
-    item.author = self
+    @items << item unless @items.include?(item)
+    item.add_author(self)
+  end
+
+  def full_name
+    "#{@first_name} #{@last_name}"
+  end
+
+  def to_hash
+    {
+      id: @id,
+      first_name: @first_name,
+      last_name: @last_name,
+      items: @items.map(&:to_hash)
+    }
+  end
+
+  class << self
+    attr_accessor :id_counter
+  end
+  @id_counter = 0
+end
+
+class Foo
+  def bar
+    Author.id_counter = 42
   end
 end
