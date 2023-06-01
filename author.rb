68d89a5c90c9d143c26c3ac11caebf7a013a68d9
Kelvin <ngotho.kelvin@yahoo.com>
2023-06-01 13:58:01 +0300

    Fix linters

--- author.rb
+++ author.rb
@@ -1,15 +1,15 @@
 class Genre
-    attr_reader :id
-    attr_accessor :name, :items
-  
-    def initialize(name, id: nil)
-      @id = id.nil? ? generate_id : id
-      @name = name
-      @items = []
-    end
-  
-    def add_item(item)
-      @items << item
-      item.genre = self
-    end
-end  
+  attr_reader :id
+  attr_accessor :name, :items
+
+  def initialize(name, id: nil)
+    @id = id.nil? ? generate_id : id
+    @name = name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.genre = self
+  end
+end
