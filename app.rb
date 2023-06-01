aef06752c149a2ec2f7b36fe5238e16128982599
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-06-01 11:18:50 +0300

    Fix linter errors

--- app.rb
+++ app.rb
@@ -18,26 +18,25 @@ class App
     type == :to_i ? input.to_i : input
   end
 
-  
-def add_book(books)
-  puts 'Enter book publisher:'
-  publisher = gets.chomp
-  puts 'Enter cover state (good/bad):'
-  cover_state = gets.chomp
-  book = Book.new(publisher, cover_state)
-  books << book
-  book
-end
+  def add_book(books)
+    puts 'Enter book publisher:'
+    publisher = gets.chomp
+    puts 'Enter cover state (good/bad):'
+    cover_state = gets.chomp
+    book = Book.new(publisher, cover_state)
+    books << book
+    book
+  end
 
-def list_books(books)
-  puts 'List of all books:'
-  books.each do |book|
-    puts "Book ID: #{book.id},
+  def list_books(books)
+    puts 'List of all books:'
+    books.each do |book|
+      puts "Book ID: #{book.id},
     Publisher: #{book.publisher},
     Cover State: #{book.cover_state},
     Archived: #{book.archived}"
+    end
   end
-end
 
   def list_labels
     puts 'Listing all labels:'
