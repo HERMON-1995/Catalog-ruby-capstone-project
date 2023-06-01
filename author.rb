22b77ee58b50b1f5a8b37f673dc10c2e2437fe91
Kelvin <ngotho.kelvin@yahoo.com>
2023-06-01 14:41:03 +0300

    Add author class

--- author.rb
+++ author.rb
@@ -1,15 +1,15 @@
-class Genre
-  attr_reader :id
-  attr_accessor :name, :items
+class Author
+  attr_accessor :first_name, :last_name, :id, :items
 
-  def initialize(name, id: nil)
-    @id = id.nil? ? generate_id : id
-    @name = name
+  def initialize(first_name, last_name, id: nil)
+    @id = id.nil? ? Random.rand(1..1000) : id
+    @first_name = first_name
+    @last_name = last_name
     @items = []
   end
 
   def add_item(item)
     @items << item
-    item.genre = self
+    item.author = self
   end
 end
