38ebc80ad39e211434dffef3abaa2cbc9fc2753c
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-06-02 19:15:52 +0300

    Add can_be_archived test and to_h test

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -23,4 +23,48 @@ RSpec.describe Book do
       expect(book.cover_state).to eq 'Best'
     end
   end
+
+  describe '#can_be_archived?' do
+    context 'when the book cover state is "bad"' do
+      before do
+        allow(book).to receive(:super).and_return(false)
+        book.cover_state = 'bad'
+      end
+
+      it 'returns true' do
+        expect(book.can_be_archived?).to be true
+      end
+    end
+
+    context 'when the book is not archived and the cover state is not "bad"' do
+      before do
+        allow(book).to receive(:super).and_return(false)
+        book.cover_state = 'good'
+      end
+
+      it 'returns false' do
+        expect(book.can_be_archived?).to be false
+      end
+    end
+  end
+
+  describe '#to_h' do
+    let(:expected_hash) do
+      {
+        id: book.id,
+        publisher: book.publisher,
+        cover_state: book.cover_state,
+        genre: nil,
+        author: nil,
+        source: nil,
+        label: nil,
+        publish_date: nil,
+        archived: false
+      }
+    end
+
+    it 'returns a hash representation of the book' do
+      expect(book.to_h).to eq expected_hash
+    end
+  end
 end
